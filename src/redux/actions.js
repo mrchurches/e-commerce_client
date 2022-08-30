fe74dddd7b8de175bc822a8aea3fe18a869fdfe7
mrchurches <laureanoiglesias34@gmail.com>
2022-08-30 16:45:33 -0300

    error action

--- src/redux/actions.js
+++ src/redux/actions.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const GET_ALL_PRODUCTS = "GET_ALL_PRODUCTS";
+export const GET_ALL_PRODUCTS = "GET_ALL_PRODUCTS";
 
 export function getAllVideogames(){
     return function(dispatch){
