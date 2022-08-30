35647802425df01ef62e61b331b33f80b99eb392
mrchurches <laureanoiglesias34@gmail.com>
2022-08-30 20:29:44 -0300

    .

--- src/Components/LandingPage/LandingPage.jsx
+++ src/Components/LandingPage/LandingPage.jsx
@@ -3,12 +3,26 @@ import NavBar from "../NavBar/NavBar.jsx";
 import SideBar from '../SideBar/SideBar.jsx';
 
 
+// const LandingPage = () => {
+
+
+//   return (
+//     <div>
+//       <h1>hola soy landing</h1>
+//     {/* <NavBar/>  
+//     <SideBar/> */}
+//     </div>
+//   )
+// }
+
+// export default LandingPage
+
 export default function LandingPage(){
-    return (
-    <div>
-      <h1>hola soy landing</h1>
-    <NavBar/>  
-    {/* <SideBar/> */}
-    </div>
-  )
+  return (
+  <div>
+    <h1>hola soy landing</h1>
+  <NavBar/>  
+  {/* <SideBar/> */}
+  </div>
+)
 }
