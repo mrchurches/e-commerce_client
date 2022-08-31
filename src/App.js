245b3449441dd0082ebb107aa0f28cf5d7eca244
mrchurches <laureanoiglesias34@gmail.com>
2022-08-31 09:43:19 -0300

    .

--- src/App.js
+++ src/App.js
@@ -1,9 +1,7 @@
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import './App.css';
-import CreateUser from './Components/CreateUser/CreateUser';
 import LandingPage from './Components/LandingPage/LandingPage';
-import Login from './Components/Login/Login';
-import ShoppingCart from './Components/ShoppingCart/ShoppingCart';
+
 
 function App() {
   return (
@@ -11,9 +9,9 @@ function App() {
     <div className="App">
       <Switch>
         <Route path="/" component={LandingPage} />
-        <Route path="/shopping_cart" component={ShoppingCart} />
+        {/* <Route path="/shopping_cart" component={ShoppingCart} />
         <Route path="/create_user"component={CreateUser} />
-        <Route path="/login" component={Login} />
+        <Route path="/login" component={Login} /> */}
       </Switch>
     </div>
     </BrowserRouter>
