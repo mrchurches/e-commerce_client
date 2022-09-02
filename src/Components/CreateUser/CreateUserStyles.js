5464ab0ea42699ccbe5e0bce8646295796bacbae
aalred <aalex.red1@gmail.com>
2022-09-02 11:58:36 -0500

    create variable to export classes

---
+++ src/Components/CreateUser/CreateUserStyles.js
@@ -0,0 +1,37 @@
+// eslint-disable-next-line
+export const styleInput = 'block \
+py-2.5 \
+    px-0 \
+    w-full \
+    text-sm \
+    text-gray-900 \
+    bg-red-500 \
+    border-0 \
+    border-b-2 \
+    border-gray-300 \
+    appearance-none \
+    is-valid\
+    form-control \
+    dark:text-black \
+    dark:border-gray-600 \
+    dark:focus:border-blue-500 \
+    focus:outline-none focus:ring-0 \
+    focus:border-blue-600 \
+    peer'
+    
+    // eslint-disable-next-line
+export const submit = "text-white \
+bg-blue-70 \
+hover:bg-blue-800 \
+focus:ring-4 \
+focus:outline-none \
+focus:ring-blue-300 \
+font-medium \
+rounded-lg \
+text-sm \
+w-full \
+sm:w-auto \
+px-5 py-2.5 \
+text-center \
+dark:bg-blue-600 dark:hover:bg-blue-700 \
+dark:focus:ring-blue-800"
