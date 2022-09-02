ced8754679d15d09fedd1a04c885986b555e67ab
aalred <aalex.red1@gmail.com>
2022-09-02 11:50:56 -0500

    added path route

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import './App.css';
 import LandingPage from './Components/LandingPage/LandingPage';
 import Footer from "./Components/Footer/Footer";
+import CreateUser from './Components/CreateUser/CreateUser';
 
 
 function App() {
@@ -10,8 +11,8 @@ function App() {
     <div className="App">
       <Switch>
         <Route path="/" component={LandingPage} />
-        {/* <Route path="/shopping_cart" component={ShoppingCart} />
         <Route path="/create_user"component={CreateUser} />
+    {/*     <Route path="/shopping_cart" component={ShoppingCart} />
         <Route path="/login" component={Login} /> */}
       </Switch>
     </div>
