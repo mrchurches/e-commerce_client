5ad6599f007bdbd66ffe9feb3394d2d8c78376cb
mrchurches <laureanoiglesias34@gmail.com>
2022-09-02 22:48:24 -0300

    login styls

--- src/Components/Login/Login.jsx
+++ src/Components/Login/Login.jsx
@@ -25,28 +25,58 @@ const Login = () => {
   }
   
   return (
-    <div>
-      <div>
-        <h2>Login</h2>
-      </div>
-      <div>
+    <div class="">
+      {/* <div>
         <form onSubmit={handleSubmit}>
-        <label>Email:</label>
-        <input type="email" placeholder="example@examplemail.com" onChange={handleChange} value={user.email} name="email"/>
-        <br />
-        <label>Password:</label>
-        <input type="password" onChange={handleChange} value={user.password} name="password"/> 
-        <br />
-        <input disabled={disabled} type="submit" value="Log in" />
+          <div>
+            <label>Email:</label>
+            <input type="email" placeholder="example@examplemail.com" onChange={handleChange} value={user.email} name="email"/>
+          </div>
+          <div>
+            <label>Password:</label>
+            <input type="password" onChange={handleChange} value={user.password} name="password"/> 
+            <input disabled={disabled} type="submit" value="Log in" />
+          </div>
         </form>
       </div>
       <div>
         <h4>don't have an account?</h4>
         <Link to="/create_user"><span>Create one!</span></Link>
+      </div> */}
+<div class="card" style={{width: '18rem'}}>
+  <div class="card-body">
+    <div class="mb-3">
+      <label for="exampleFormControlInput1" class="form-label">Email address</label>
+      <input type="email" class="form-control" id="exampleFormControlInput1" placeholder="name@example.com" />
+    </div>
+    <div class="mb-3">
+      <label for="exampleFormControlTextarea1" class="form-label">Example textarea</label>
+      <textarea class="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
+    </div>
+
+  </div>
+</div>
+      
+        {/* <form>
+      <div class="mb-3">
+        <label for="exampleInputEmail1" class="form-label">Email address</label>
+        <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" />
+        <div id="emailHelp" class="form-text">We'll never share your email with anyone else.</div>
       </div>
+      <div class="mb-3">
+        <label for="exampleInputPassword1" class="form-label">Password</label>
+        <input type="password" class="form-control" id="exampleInputPassword1" />
+      </div>
+      <div class="mb-3 form-check">
+        <input type="checkbox" class="form-check-input" id="exampleCheck1">
+        <label class="form-check-label" for="exampleCheck1">Check me out</label>
+      </div>
+      <button type="submit" class="btn btn-primary">Submit</button>
+    </form> */}
+
     </div>
     
-  )
+    )
 }
 
 export default Login
