0d2cf388d0fb1379f110e889d69df8970d85459d
aalred <aalex.red1@gmail.com>
2022-09-03 16:26:40 -0500

    deleted classes not incorporated

--- src/Components/CreateUser/CreateUserStyles.js
+++ src/Components/CreateUser/CreateUserStyles.js
@@ -32,21 +32,3 @@ px-5 py-2.5 \
 text-center \
 dark:bg-blue-600 dark:hover:bg-blue-700 \
 dark:focus:ring-blue-800"
-
-
-const inputError = 'block \
-    py-2.5 \
-    px-0 \
-    w-full \
-    text-sm \
-    text-gray-900 \
-    border-0 \
-    border-b-2 \
-    border-gray-300 \
-    appearance-none \
-    dark:text-black \
-    dark:border-gray-600 \
-    dark:focus:border-blue-500 \
-    focus:outline-none focus:ring-0 \
-    focus:border-blue-600 \
-    peer'
