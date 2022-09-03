99b4f988c26aa3b63f85bbd2b0933c2a81eb8509
mrchurches <laureanoiglesias34@gmail.com>
2022-09-03 13:58:27 -0300

    solve links problem

--- src/Components/Cards/ProductCard/ProductCard.jsx
+++ src/Components/Cards/ProductCard/ProductCard.jsx
@@ -24,7 +24,7 @@ export default function ProductCard({ id, name, img, rating, platforms, /* price
           <Link to={`/detail${id}`}>
             <div class="w-full max-w-sm bg-white rounded-lg shadow-md dark:bg-gray-800 dark:border-gray-700">
               <a href="#">
-                <img class="p-8 rounded-t-lg" src={img} alt="product image" />
+                <img class="p-8 rounded-t-lg" src={img} alt="product img" />
               </a>
               <div class="px-5 pb-5">
                 <a href="#">
@@ -40,7 +40,7 @@ export default function ProductCard({ id, name, img, rating, platforms, /* price
                 </div>
                 <div class="flex justify-between items-center">
                   <span class="text-3xl font-bold text-gray-900 dark:text-white">{Math.random() * 100}</span>
-                  <a href="#" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Add to cart</a>
+                  <p class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Add to cart</p>
                 </div>
               </div>
             </div>
@@ -52,10 +52,10 @@ export default function ProductCard({ id, name, img, rating, platforms, /* price
         
         <div class="card">
           <Link to={`/detail/${id}`}>
-            <img class="card-img-top" style={{width: '18rem', height: '10rem'}} src={img} alt="product image" />
+            <img class="card-img-top" style={{width: '18rem', height: '10rem'}} src={img} alt="product img" />
           </Link>
           <div class="card-body">
-            <Link to={`/detail/${id}`}>
+            <Link to={`/detail/${id}`} style={{textDecoration: "none"}}>
               <h5 class="card-title">{name}</h5>
             </Link>
             {/*<div class="ratings">
