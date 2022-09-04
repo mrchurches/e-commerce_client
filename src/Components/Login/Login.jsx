39eb4710fe338d1291b241dc664cb78b26fe8ab8
aalred <aalex.red1@gmail.com>
2022-09-03 21:44:54 -0500

    updated file

--- src/Components/Login/Login.jsx
+++ src/Components/Login/Login.jsx
@@ -1,6 +1,5 @@
 
-import style from "./Login.module.css"
-import { btnGLogo } from "./LoginStyle";
+import "./Login.css"
 
 import React, { useEffect } from 'react'
 import { useState } from "react";
@@ -15,23 +14,23 @@ import { useSelector } from "react-redux";
 
 const Login = () => {
   const [user, setUser] = useState({ username: "", password: "" }),
-  [userGet, setUserGet] = useState({ userNExists: false, failedLog:false, userBan:false }),
-  [disabled, setDisabled] = useState(true),
-   dispatch = useDispatch()
+    [userGet, setUserGet] = useState({ userNExists: false, failedLog: false, userBan: false }),
+    [disabled, setDisabled] = useState(true),
+    dispatch = useDispatch()
 
   const { userAuth } = useSelector(state => {
     return { userAuth: state.users }
   })
 
-  function resetStates(){
+  function resetStates() {
     setUser({ username: "", password: "" });
     setDisabled(true)
   }
-  
+
   useEffect(() => {
     if (user.username && user.password) {
       setDisabled(false)
-    }else{
+    } else {
       setDisabled(true)
     }
   }, [user])
@@ -39,57 +38,57 @@ const Login = () => {
   function handleChange(e) {
     setUser({ ...user, [e.target.id]: e.target.value })
     if (e.target.id === 'email') {
-      setUserGet((i) => ({...i,  userNExists: false, userBan:false}))
-    }else{
-      setUserGet((i) => ({...i, failedLog:false}))
+      setUserGet((i) => ({ ...i, userNExists: false, userBan: false }))
+    } else {
+      setUserGet((i) => ({ ...i, failedLog: false }))
     }
   }
 
   async function handleSubmit(e) {
     e.preventDefault();
     const userExist = await findEmail(user.username);
-    if(!userExist){
-      setUserGet((i) => ({...i, userNExists: true}));
-    } else if(userExist.isBanned){
-      setUserGet((i) => ({...i, userBan: true}));
-    }else{
+    if (!userExist) {
+      setUserGet((i) => ({ ...i, userNExists: true }));
+    } else if (userExist.isBanned) {
+      setUserGet((i) => ({ ...i, userBan: true }));
+    } else {
       const info = await dispatch(postUsers(user));
-      !info.includes('Welcome') && setUserGet((i) => ({...i, failedLog: true}));
+      !info.includes('Welcome') && setUserGet((i) => ({ ...i, failedLog: true }));
     }
     resetStates()
   }
   return (
-    <div class="d-flex justify-content-center ">
-      {userAuth.user && <Redirect to='/'/>}
+    <div class="mt-5 d-flex justify-content-center ">
+      {userAuth.user && <Redirect to='/' />}
       <div class="card shadow-lg p-3 mb-5 bg-body rounded" style={{ width: '18rem' }}>
         <form onSubmit={(e) => handleSubmit(e)}>
           <div class="mb-3">
             <label for="exampleInputEmail1" class="form-label">Email address</label>
             <input type="email" id="username" class="form-control" aria-describedby="emailHelp" placeholder="example@examplemail.com" onChange={handleChange} value={user.username} name="username" />
-            {userGet.userNExists && <p>Email addres invalid</p> }
-            {userGet.userBan && <p>Email addres are banned</p> }
-            <div id="emailHelp" class="form-text">We'll never share your email with anyone else.</div>
+            {userGet.userNExists && <p>Email addres invalid</p>}
+            {userGet.userBan && <p>Email addres are banned</p>}
+            <small id="emailHelp" class="form-text">We'll never share your email with anyone else.</small>
           </div>
           <div class="mb-3">
             <label for="exampleInputPassword1" class="form-label">Password</label>
             <input type="password" class="form-control" id="password" onChange={handleChange} value={user.password} name="password" />
-            {userGet.failedLog && <p>Password are invalid</p> }
+            {userGet.failedLog && <p>Password are invalid</p>}
           </div>
           <input disabled={disabled} type="submit" class="btn btn-primary" value="Login" />
-        </form>
+        </form><br />
+
         <div>
-          <p class="form-label">don't have an account?</p>
+          <small class="form-label">don't have an account?</small><br />
           <Link to="/create_user">
             <span class="btn btn-primary">Create one!</span>
           </Link>
         </div>
         <p>OR</p>
-        <div>
-          <a href={`${REACT_APP_URL}/login/auth/google`} class={btnGLogo} >
-            <button type="button"
-              viewBox="0 0 320 512"><img src={gLogo} class="h-px inline-block" id='' />
-              Sign in with google
-            </button>
+        <div class="btnLogo">
+          <a class={"linkA"} href={`${REACT_APP_URL}/login/auth/google`}>
+            <img src={gLogo} class="" id='' alt='googleButton' />
+            <small class="form-label">Sign in with google</small><br />
+            <br />
           </a>
         </div>
       </div>
