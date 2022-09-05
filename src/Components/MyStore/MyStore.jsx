ab0126a85af4265ac6db295c22a057aa666ac241
mrchurches <laureanoiglesias34@gmail.com>
2022-09-04 23:33:13 -0300

    add template in myStore

--- src/Components/MyStore/MyStore.jsx
+++ src/Components/MyStore/MyStore.jsx
@@ -2,7 +2,34 @@ import React from 'react'
 
 const MyStore = () => {
   return (
-    <div>MyStore</div>
+    <div class="d-flex p-2  justify-content-center">
+      <div class="list-group align-self-center">
+        <a href="#" class="list-group-item list-group-item-action active" aria-current="true">
+          <div class="d-flex w-100 justify-content-between">
+            <h5 class="mb-1">Assesins creed</h5>
+            <small>3 days ago</small>
+          </div>
+          <p class="mb-1">Some placeholder content in a paragraph.</p>
+          <small>And some small print.</small>
+        </a>
+        <a href="#" class="list-group-item list-group-item-action">
+          <div class="d-flex w-100 justify-content-between">
+            <h5 class="mb-1">Crash</h5>
+            <small class="text-muted">5 days ago</small>
+          </div>
+          <p class="mb-1">Some placeholder content in a paragraph.</p>
+          <small class="text-muted">And some muted small print.</small>
+        </a>
+        <a href="#" class="list-group-item list-group-item-action">
+          <div class="d-flex w-100 justify-content-between">
+            <h5 class="mb-1">Forza horizon</h5>
+            <small class="text-muted">1 month ago</small>
+          </div>
+          <p class="mb-1">Some placeholder content in a paragraph.</p>
+          <small class="text-muted">And some muted small print.</small>
+        </a>
+      </div>
+    </div>
   )
 }
 
