41474be0782adfaecdadaf8a625cc25f30e6b472
facucorona <coronafacundo@gmail.com>
2022-09-06 18:00:55 -0300

    pasarela component

---
+++ src/Components/Pasarela/Pasarela.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+function Pasarela() {
+
+    useEffect(() => {
+
+    }, [input])
+
+    return (
+        <form id={FORM_ID} method="GET" />
+    )
+}
+
+export default Pasarela
