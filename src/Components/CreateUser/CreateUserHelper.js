1921b6abdce3a2b81f3f606e5d818529f695f3e7
aalred <99151344+aalred@users.noreply.github.com>
2022-09-06 21:52:25 -0500

    Update CreateUserHelper.js

--- src/Components/CreateUser/CreateUserHelper.js
+++ src/Components/CreateUser/CreateUserHelper.js
@@ -50,7 +50,7 @@ export async function existsUsername(username) {
 };
 
 export async function findEmail(email) {
-  const response = await axios.get(`${REACT_APP_URL}/user/find/email/${email}`);
+  const response = await axios.get(`${REACT_APP_URL}user/find/email/${email}`);
   return response.data.user
 };
 
