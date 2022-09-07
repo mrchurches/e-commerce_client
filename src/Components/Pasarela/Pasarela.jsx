57577d33f917621298f06eaf2b41bb00b712a237
facucorona <coronafacundo@gmail.com>
2022-09-06 21:36:44 -0300

    pasarela - botón

--- src/Components/Pasarela/Pasarela.jsx
+++ src/Components/Pasarela/Pasarela.jsx
@@ -1,25 +1,61 @@
 import React, { useEffect, useState } from 'react';
 import { useParams } from "react-router-dom";
 import axios from "axios"
-import MercadoPago from "mercadopago"
-const { PUBLIC_KEY } = process.env
+// // import MercadoPago from "mercadopago"
+// const { PUBLIC_KEY } = process.env
+
+// const FORM_ID = 'payment-form';
+// const mp = new MercadoPago(PUBLIC_KEY);
+
+// export default function Pasarela() {
+//     const { id } = useParams(); // id de producto
+//     const [preferenceId, setPreferenceId] = useState(null);
+
+//     const mp = new MercadoPago(PUBLIC_KEY);
+//     console.log("mp")
+//     console.log(mp)
+//     const bricksBuilder = mp.bricks();
+
+
+
+
+//     return (
+//         <div id="cardPaymentBrick_container"></div>
+//     );
+// }
+
+
 
 const FORM_ID = 'payment-form';
-const mp = new MercadoPago(PUBLIC_KEY);
 
-export default function Pasarela() {
+export default function Product() {
     const { id } = useParams(); // id de producto
     const [preferenceId, setPreferenceId] = useState(null);
 
-    const mp = new MercadoPago(PUBLIC_KEY);
-    console.log("mp")
-    console.log(mp)
-    const bricksBuilder = mp.bricks();
+    useEffect(() => {
+        // luego de montarse el componente, le pedimos al backend el preferenceId
+        axios.post('http://localhost:3001/payment', { productId: id })
+            .then((order) => {
+                console.log(order)
+                order = order.data
+                setPreferenceId(order.preferenceId);
+            });
+    }, [id]);
 
-
-
+    useEffect(() => {
+        if (preferenceId) {
+            // con el preferenceId en mano, inyectamos el script de mercadoPago
+            const script = document.createElement('script');
+            script.type = 'text/javascript';
+            script.src =
+                'https://www.mercadopago.com.ar/integrations/v1/web-payment-checkout.js';
+            script.setAttribute('data-preference-id', preferenceId);
+            const form = document.getElementById(FORM_ID);
+            form.appendChild(script);
+        }
+    }, [preferenceId]);
 
     return (
-        <div id="cardPaymentBrick_container"></div>
+        <form id={FORM_ID} method="GET" />
     );
 }
