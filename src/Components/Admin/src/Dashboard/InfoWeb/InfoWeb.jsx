7356d26e77a0da2d8123e7ddd8df00d172cc813f
Mario Vidal <mario.josuevp@gmail.com>
2022-09-11 01:58:46 +0200

    dashboard

---
+++ src/Components/Admin/src/Dashboard/InfoWeb/InfoWeb.jsx
@@ -0,0 +1,32 @@
+import React from 'react'
+import style from './infoweb.module.css'
+
+export default function InfoWeb({items}) {
+
+  
+  const baneados = items.allUsers.filter(e => e.isBanned === true)
+  const Admin = items.allUsers.filter(e => e.isAdmin === true)
+  
+  return (
+    <div>
+        <div class="card border-secondary mb-3" style={{ maxwidth: "18rem;" }}>
+            <div class="card-header">Usuarios Registrados</div>
+            <div class="card-body text-secondary">
+                <h3 class="card-text">{items.allUsers.length}</h3>
+            </div>
+        </div>
+        <div class="card border-secondary mb-3" style={{ maxwidth: "18rem;" }}>
+            <div class="card-header">Usuarios Baneados</div>
+            <div class="card-body text-secondary">
+                <h3 class="card-text">{baneados.length}</h3>
+            </div>
+        </div>
+        <div class="card border-secondary mb-3" style={{ maxwidth: "18rem;" }}>
+            <div class="card-header">Administradores</div>
+            <div class="card-body text-secondary">
+                <h3 class="card-text">{Admin.length}</h3>
+            </div>
+        </div>
+    </div>
+  )
+};
