f2773ba4eee97c2cf26a2f797ab558472ee720f4
linker <diegolucas142@gmail.com>
2022-09-10 18:53:17 -0300

    intento de localstorage

--- src/Components/ShoppingCart/ShoppingCart.jsx
+++ src/Components/ShoppingCart/ShoppingCart.jsx
@@ -1,17 +1,41 @@
 import "./ShoppingCart.css";
 import { useSelector } from "react-redux";
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import Checkout from '../Checkout/Checkout'
 import ProductCard from "../Cards/ProductCard/ProductCard";
 
 export default function ShoppingCart() {
 let cart = useSelector(state=>state.cart);
 let games = useSelector(state=>state.products2);
-let filterGames=[];
-cart.forEach(e=>{
-let fg = games.filter((f)=>e===f.id);
-filterGames.push(fg[0])
+let [saludo, setSaludo]=useState("hola")
+const cartLocal = JSON.parse(localStorage.getItem("item" || "[]"))
+let [carrito, setCarrito]=useState(cartLocal)
+let filterGames=[]; //juewgos dentro del carrito.
+cart.forEach(e=>{ //los juegos añadidos al carrito, los separa de los 120 juegos y los mete en filtergames
+    let fg = games.filter((f)=>e===f.id); //compara la id de cart con las id de db pushea las que matchean a filtergames
+    filterGames.push(fg[0])
 })
+
+//carga los juegos al estado local item, luego los va a setear en local storage
+//cuando nuestro estado cambie, va a añadir al local storage
+useEffect(()=>{ 
+    localStorage.setItem("item", JSON.stringify(carrito))
+    typeof id_user === "string" ? filterGames : cartLocal
+    cart.forEach(e=>{
+        let fg = games.filter((f)=>e===f.id);
+        fg.forEach(e=>setItem(e))
+    })
+    //console.log(item) //vacio //vacio // encuentra
+},[item]);
+
+//trae los juegos de local storage
+useEffect(()=>{
+    const items = JSON.parse(localStorage.getItem("item"));
+    console.log(items)
+    if(item){
+        setItem(items)
+    }
+},[])
 // asitiene que se el juego= {
 //     title: "Mi producto",
 //     unit_price: 100,
@@ -27,7 +51,7 @@ let gamesCO= filterGames.map(e=>{
 
 let forCheckout = { items: gamesCO };
 
-console.log(filterGames)
+//console.log(filterGames)
     return (
         <div class="d-flex flex-column vh-100  align-items-center">
             <div class="alert alert-dark w-50">
