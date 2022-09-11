178338f28b2689da9342c5077bf26fafb4ce58e8
aalred <aalex.red1@gmail.com>
2022-09-10 22:21:17 -0500

    updated files to verification

--- src/Components/NavBar/NavBarHelper.js
+++ src/Components/NavBar/NavBarHelper.js
@@ -1,6 +1,5 @@
 import axios from "axios";
 
-//import { REACT_APP_URL } from "../CreateUser/CreateUserHelper";
 const { REACT_APP_URL } = process.env;
 
 export async function logout(){
@@ -11,10 +10,4 @@ export async function logout(){
     } catch (error) {
         console.log(error)
     }
-}
-
-export function deleteCookies() {
-    document.cookie.split(";").forEach(function (c) {
-        document.cookie = c.replace(/^ +/, "").replace(/=.*/, "=;expires=" + new Date().toUTCString() + ";path=/");
-    });
 }
