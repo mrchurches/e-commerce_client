1812f14457cd6004cb64a1407c0d7e0d3c2de4ec
genti91 <franco.g7@hotmail.com>
2022-09-12 02:53:01 -0300

    fix

--- src/Components/Admin/src/Users/Users.jsx
+++ src/Components/Admin/src/Users/Users.jsx
@@ -6,7 +6,7 @@ import { useState } from 'react'
 import Swal from 'sweetalert2'
 /* import { banUsersss } from './banUser.js' */
 import { getAllUsers, filter_bannedAdmin, byUserName, bann_unBann, makeAdmin} from '../../../../redux/actions'
-
+//
 export default function Users({setRender}) {
   
   const bannedOn = ["Banned", "Admin", "All"]
@@ -207,8 +207,10 @@ Swal.fire({
   }
 
   return (
+
     <div className='contenedorUsers1'>
     <div className='contenedorViejo123'>
+
       
       <h2>Users</h2>
 
