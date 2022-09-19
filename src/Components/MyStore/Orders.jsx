5692b7846324a91947ae4fd758c0b27e814de783
mrchurches <laureanoiglesias34@gmail.com>
2022-09-19 00:41:21 -0300

    my store

---
+++ src/Components/MyStore/Orders.jsx
@@ -0,0 +1,42 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+
+function Orders({orders, games}) {
+  console.log(games)
+  console.log(orders)
+  return  (
+
+    <div className='testAdminUser m-5 p-5'>
+    
+    <h2 style={{color:"black"}} >Orders</h2>
+    <div class='container' className ='tablefixed1234'>
+      <div  class='row'>  
+        <table  class="table table-striped tabled-striped table-condensend table-fixed table-bordered text-sm p-3">
+          <thead>
+                <tr class="text-sm">
+                    <th class="header col-1" scope="col-10">Order id</th>
+                    <th class="header col-1" scope="col">Game</th>
+                    <th class="header col-1" scope="col">Date of purchase</th>
+                    <th class="header col-1" scope="col">Price</th>
+                </tr>
+            </thead>
+            <tbody  >
+              {
+                orders&&orders.map((e,index)=>(
+                  <tr key={index}>
+                      <td>{e.id}</td>
+                      <td><Link to={`/detail/${games[index].id}`}>{e.game_name}</Link></td>
+                      <td>{e.createdAt}</td>
+                      <td>{e.price}</td>
+                  </tr>
+                ))
+              }
+            </tbody>
+        </table>
+      </div>
+    </div>    
+  </div>
+)
+}
+
+export default Orders
